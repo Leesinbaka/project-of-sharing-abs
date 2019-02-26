eef9fd8815fc4417f7a16c50a960ec498877decc
Leesinbaka <kenneth72355700@gmail.com>
2019-02-26 19:11:01 +0800

    Day 2
    Hello world AND
    Firstpage 的測試

--- 2019專題/Django/sharingabs/app/views.py
+++ 2019專題/Django/sharingabs/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello")
+
+
+
+from django.shortcuts import render
+
+
+def firsthtml(request):
+    return render(request,"firstpage.html",locals())
