a5a0424223b58dbc371ba16523a3a91321d380ea
Leesinbaka <kenneth72355700@gmail.com>
2019-02-27 16:41:57 +0800

    test array
    回傳dictationary

--- 2019專題/Django/sharingabs/app/views.py
+++ 2019專題/Django/sharingabs/app/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
-
+from datetime import datetime
 def index(request):
     return HttpResponse("Hello")
 
 
 
 from django.shortcuts import render
-
-
+words = ["cat","dog","hello world"]#用來測試
 def firsthtml(request):
+    now = datetime.now()
+    haha ={'title':words[1]}
     return render(request,"firstpage.html",locals())
