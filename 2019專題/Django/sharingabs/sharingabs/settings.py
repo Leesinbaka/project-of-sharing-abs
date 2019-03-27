2f5bbe087dac26852c340daa55cfebe8520c6a8c
Leesinbaka <kenneth72355700@gmail.com>
2019-03-27 10:41:22 +0800

    add image to our web

--- 2019專題/Django/sharingabs/sharingabs/settings.py
+++ 2019專題/Django/sharingabs/sharingabs/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR,'static'),   
-]
+SITE_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+STATICFILES_DIRS = (
+("css", os.path.join(STATIC_ROOT, 'css')),
+("js", os.path.join(STATIC_ROOT, 'js')),
+("images", os.path.join(STATIC_ROOT, 'images')),
+)
